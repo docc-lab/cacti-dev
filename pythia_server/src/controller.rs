182e025f42a2bead3362e27030f3a3b9e115b9ca
tome215 <tome215@gmail.com>
2024-04-08 13:03:21 -0400

    re-enabling logging on pythia-server - 4

--- pythia_server/src/controller.rs
+++ pythia_server/src/controller.rs
@@ -89,14 +89,15 @@ impl OSProfilerController {
         //     tracepoint, request_type, path
         // );
 
-        let data = format!(
-            "PFTR - PATH FOR TRACEPOINT {:?} AND REQUEST TYPE {:?} IS {:?}",
-            tracepoint, request_type, path
-        );
+        // let data = format!(
+        //     "PFTR - PATH FOR TRACEPOINT {:?} AND REQUEST TYPE {:?} IS {:?}",
+        //     tracepoint, request_type, path
+        // );
         // let mut f = File::create("/tmp/yeet/lmao.txt").expect("Unable to create file");
         // f.write_all(data.as_bytes()).expect("Unable to write data");
         println!();
-        println!(data);
+        println!("PFTR - PATH FOR TRACEPOINT {:?} AND REQUEST TYPE {:?} IS {:?}",
+                 tracepoint, request_type, path);
         println!();
 
         match File::create(&path) {
